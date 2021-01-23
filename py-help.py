b3ece4e1d5f5c4bafd60f88e46deae3f28aa91ce
Mr-S-Mirzoev <mrsergeymirzoev@gmail.com>
2021-01-23 13:00:23 +0300

    Removed a helper file

--- py-help.py
+++
@@ -1,8 +0,0 @@
-from os import listdir, path
-
-prefix = "/usr/include/x86_64-linux-gnu/qt5/"
-
-for name in listdir(prefix):
-    fullpath = path.join(prefix, name)
-    if path.isdir(fullpath):
-        print("\"{}\",".format(fullpath))
